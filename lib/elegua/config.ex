237de5ea78f086e645e83b594e6d8c5e187463ab
Zura Guerra <guerrazuramx@gmail.com>
2016-02-03 22:33:03 -0600

    Change config vars to constants

--- lib/elegua/config.ex
+++ lib/elegua/config.ex
@@ -1,20 +1,20 @@
 defmodule Elegua.Config do
-	user_model = Application.get_env(:elegua, :user, YourApp.User)
-	email_field = Application.get_env(:elegua, :email_field, :email)
-	username_field = Application.get_env(:elegua, :username_field, :username)
-	password_field = Application.get_env(:elegua, :password_field, :password)
+	@user_model Application.get_env(:elegua, :user, YourApp.User)
+	@email_field Application.get_env(:elegua, :email_field, :email)
+	@username_field Application.get_env(:elegua, :username_field, :username)
+	@password_field Application.get_env(:elegua, :password_field, :password)
 
-	app_repo = Application.get_env(:elegua, :app_repo, YourApp.Repo)
+	@app_repo Application.get_env(:elegua, :app_repo, YourApp.Repo)
 
 	def required_fields do
-		[email_field, username_field, password_field]
+		[@email_field, @username_field, @password_field]
 	end
 
 	def app_repo do
-		app_repo
+		@app_repo
 	end
 
 	def password_field do
-		password_field
+		@password_field
 	end
 end
--- lib/elegua/register.ex
+++ lib/elegua/register.ex
@@ -4,12 +4,12 @@ defmodule Elegua.Register do
   alias Elegua.Config
   alias Elegua.Password
 
-  password_field = Config.password_field
+  @password_field Config.password_field
 
   def create(changeset) do
-  	hashed_password = Password.hash(changeset.params[Atom.to_string(password_field)])
+  	hashed_password = Password.hash(changeset.params[Atom.to_string(@password_field)])
   	changeset
-  	|> put_change(password_field, hashed_password)
+  	|> put_change(@password_field, hashed_password)
   	|> Config.app_repo.insert
   end
 
