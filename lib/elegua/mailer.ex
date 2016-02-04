4c98c870084270da85b62ebc08f6f3a48a5189d2
Zura Guerra <guerrazuramx@gmail.com>
2016-02-04 01:06:20 -0600

    Send verification mails

--- lib/elegua/mailer.ex
+++ lib/elegua/mailer.ex
@@ -4,6 +4,18 @@ defmodule Elegua.Mailer do
 	  domain: Application.get_env(:mailgun, :domain),
 	  key: Application.get_env(:mailgun, :key)
 
-	
+	def send_verification_email(to, from, subject, {:text, content}) do
+	  send_email to: to,
+	    from: from,
+	    subject: subject,
+	    text: text
+	end
+
+	def send_verification_email(to, from, subject, {:html, content}) do
+	  send_email to: to,
+	    from: from,
+	    subject: subject,
+	    html: html
+	end
 
 end
