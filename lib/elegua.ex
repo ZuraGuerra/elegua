8aebaf7965408368f6e4b8c219c841a5fba5b6ca
Zura Guerra <guerrazuramx@gmail.com>
2016-02-04 01:21:54 -0600

    Send verification email from interface

--- lib/elegua.ex
+++ lib/elegua.ex
@@ -2,6 +2,7 @@ defmodule Elegua do
 
 	alias Elegua.Register
 	alias Elegua.Model
+	alias Elegua.Mailer
 
 	def changeset(user, params) do
 		Model.changeset(user, params)
@@ -11,7 +12,11 @@ defmodule Elegua do
 		Register.create(changeset)
 	end
 
-	def register(changeset, :verify) do
-		Register.create(changeset, :verify)
+	def register(changeset, opts) do
+		Register.create(changeset, opts)
+	end
+
+	def send_verification_mail(to, from, subject, {content_type, content}) do
+		Mailer.send_verification_mail(to, from, subject, {content_type, content})
 	end
 end
