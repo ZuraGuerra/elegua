aaf390c14d798e42610fc49a2c42971f75d9e5f7
Zura Guerra <guerrazuramx@gmail.com>
2016-02-03 22:30:27 -0600

    Create user in DB after hashing password

--- lib/elegua/register.ex
+++ lib/elegua/register.ex
@@ -1,5 +1,17 @@
 defmodule Elegua.Register do
-	
+  import Ecto.Changeset, only: [put_change: 3]
+  
+  alias Elegua.Config
+  alias Elegua.Password
+
+  password_field = Config.password_field
+
+  def create(changeset) do
+  	hashed_password = Password.hash(changeset.params[Atom.to_string(password_field)])
+  	changeset
+  	|> put_change(password_field, hashed_password)
+  	|> Config.app_repo.insert
+  end
 
 
 end
