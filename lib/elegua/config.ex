f54068e1066c6e059d0d3a67fef47180bfe15295
Zura Guerra <guerrazuramx@gmail.com>
2016-02-05 12:47:39 -0600

    Fix Config bug

--- lib/elegua/config.ex
+++ lib/elegua/config.ex
@@ -1,34 +1,33 @@
 defmodule Elegua.Config do
-	@user_model Application.get_env(:elegua, :user, YourApp.User)
-	@email_field Application.get_env(:elegua, :email_field, :email)
-	@username_field Application.get_env(:elegua, :username_field, :username)
-	@password_field Application.get_env(:elegua, :password_field, :password)
-	@verification_token_field Application.get_env(:elegua, :verification_token_field, :verification_token)
-	@is_verified_field Application.get_env(:elegua, :is_verified_field, :is_verified)
-
-	@app_repo Application.get_env(:elegua, :app_repo, YourApp.Repo)
-
 	def required_fields do
-		[@email_field, @username_field, @password_field]
+		[email_field, username_field, password_field]
 	end
 
 	def app_repo do
-		@app_repo
+		Application.get_env(:elegua, :app_repo, YourApp.Repo)
 	end
 
 	def user_model do
-		@user_model
+		Application.get_env(:elegua, :user, YourApp.User)
+	end
+
+	def email_field do
+		Application.get_env(:elegua, :email_field, :email)
+	end
+
+	def username_field do
+		Application.get_env(:elegua, :username_field, :username)
 	end
 
 	def password_field do
-		@password_field
+		Application.get_env(:elegua, :password_field, :password)
 	end
 
 	def verification_token_field do
-		@verification_token_field
+		Application.get_env(:elegua, :verification_token_field, :verification_token)
 	end
 
 	def is_verified_field do
-		@is_verified_field
+		Application.get_env(:elegua, :is_verified_field, :is_verified)
 	end
 end
