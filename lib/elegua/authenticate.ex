73cb9861af043c8582591d5d937b54e7d7c40876
Zura Guerra <guerrazuramx@gmail.com>
2016-02-09 21:30:44 -0600

    Add user authentication

---
+++ lib/elegua/authenticate.ex
@@ -0,0 +1,25 @@
+defmodule Elegua.Authenticate do
+  import String, only: [downcase: 1]
+  alias Ecto.Query
+  alias Elegua.Config
+  alias Elegua.Password
+
+  @app_repo Config.app_repo
+  @user_model Config.user_model
+
+  def authenticate({:email, email}, password) do
+    user = @app_repo.get_by(@user_model, email: downcase(email))
+    case check_credentials(user, password) do
+      true -> {:ok, user}
+      {:no_user, reason} -> {:error, :no_user}
+      :else -> {:error, :invalid_password}
+    end   
+  end
+
+  defp check_credentials(user, password) do
+    case user do
+      nil -> {:error, :no_user}
+      :else -> Password.match_passwords(password, user)
+    end
+  end
+end
--- lib/elegua/password.ex
+++ lib/elegua/password.ex
@@ -1,6 +1,11 @@
 defmodule Elegua.Password do
+  alias Comeonin.Bcrypt
 	
-	def hash(password) do
-		Comeonin.Bcrypt.hashpwsalt(password)
-	end
+  def hash(password) do
+	Bcrypt.hashpwsalt(password)
+  end
+    
+  def match_passwords(password, user) do
+    Bcrypt.checkpw(password, user.password)
+  end
 end
