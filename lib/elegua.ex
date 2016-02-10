13d9855a1b5b7820f9dbe38fcf6ae4e3ef630126
Zura Guerra <guerrazuramx@gmail.com>
2016-02-09 22:41:52 -0600

    Refactor interface

--- lib/elegua.ex
+++ lib/elegua.ex
@@ -5,27 +5,17 @@ defmodule Elegua do
 	alias Elegua.Mailer
     alias Elegua.Authenticate, as: Auth
 
-	def changeset(user, params) do
-		Model.changeset(user, params)
-	end
+	def changeset(user, params), do: Model.changeset(user, params)
 
-	def register(changeset) do
-		Register.create(changeset)
-	end
+	def register(changeset), do: Register.create(changeset)
 
-	def register(changeset, opts) do
-		Register.create(changeset, opts)
-	end
+	def register(changeset, opts), do: Register.create(changeset, opts)
 
 	def send_verification_email(to, from, subject, {content_type, content}) do
 		Mailer.send_verification_email(to, from, subject, {content_type, content})
 	end
 
-	def verify(token) do
-		Register.verify(token)
-	end
+	def verify(token), do: Register.verify(token)
 
-    def authenticate({:email, email}, password) do
-        Auth.authenticate({:email, email}, password)
-    end
+    def authenticate({:email, email}, password), do: Auth.authenticate({:email, email}, password)
 end
