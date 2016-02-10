b674107ee8625aafb30dfd61ef028e50847ae8cc
Zura Guerra <guerrazuramx@gmail.com>
2016-02-09 22:53:38 -0600

    Add login to interface

--- lib/elegua.ex
+++ lib/elegua.ex
@@ -1,21 +1,20 @@
 defmodule Elegua do
 
-	alias Elegua.Register
-	alias Elegua.Model
-	alias Elegua.Mailer
-    alias Elegua.Authenticate, as: Auth
+  alias Elegua.Register
+  alias Elegua.Model
+  alias Elegua.Mailer
+  alias Elegua.Authenticate, as: Auth
+  alias Elegua.Session
 
-	def changeset(user, params), do: Model.changeset(user, params)
+  def changeset(user, params), do: Model.changeset(user, params)
+  def register(changeset), do: Register.create(changeset)
+  def register(changeset, opts), do: Register.create(changeset, opts)
 
-	def register(changeset), do: Register.create(changeset)
+  def send_verification_email(to, from, subject, {content_type, content}) do
+	Mailer.send_verification_email(to, from, subject, {content_type, content})
+  end
+  def verify(token), do: Register.verify(token)
 
-	def register(changeset, opts), do: Register.create(changeset, opts)
-
-	def send_verification_email(to, from, subject, {content_type, content}) do
-		Mailer.send_verification_email(to, from, subject, {content_type, content})
-	end
-
-	def verify(token), do: Register.verify(token)
-
-    def authenticate({:email, email}, password), do: Auth.authenticate({:email, email}, password)
+  def authenticate({:email, email}, password), do: Auth.authenticate({:email, email}, password)
+  def login(email, password), do: Session.new(email, password)
 end
