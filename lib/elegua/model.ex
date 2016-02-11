4b864fab155ee4810e390b8075afce5be0f8b1e8
Zura Guerra <guerrazuramx@gmail.com>
2016-02-10 21:40:48 -0600

    Save user new password

--- lib/elegua/model.ex
+++ lib/elegua/model.ex
@@ -4,8 +4,10 @@ defmodule Elegua.Model do
   alias Elegua.Config
 
   @required_fields Config.base_required_fields
-  @optional_fields [Config.verification_token_field, Config.is_verified_field, Config.new_password_field]
+  @optional_fields [Config.verification_token_field, Config.is_verified_field]
   @verification_required_fields @required_fields ++ @optional_fields
+  @recovery_fields [Config.verification_token_field, Config.new_password_field]
+  @recovery_required_fields @required_fields ++ @recovery_fields ++ Config.is_verified_field
 
   def changeset(user, params \\ :empty) do
   	user
@@ -15,6 +17,11 @@ defmodule Elegua.Model do
   def verification_changeset(user, params \\ :empty) do
   	user
   	|> cast(params, @verification_required_fields, [])
+  end
+
+  def recovery_changeset(user, params \\ :empty) do
+    user
+    |> cast(params, @recovery_required_fields, [])
   end
 
 end
--- lib/elegua/recover.ex
+++ lib/elegua/recover.ex
@@ -1,14 +1,34 @@
 defmodule Elegua.Recover do
-  import Ecto.Changeset, only: [put_change: 3]
+  import Ecto.Changeset, only: [put_change: 3, change]
   import String, only: [downcase: 1]
+  import Atom, only: [to_string: 1]
   import Ecto.Query
-  alias Elegua.{Mailer, Config}
+  alias Elegua.{Mailer, Config, Model, Password}
   
   @app_repo Config.app_repo
   @user_model Config.user_model
+  @new_password Config.new_password
+  @verification_token Config.verification_token
 
-  def send_link(email) do
+  def new_password(email, password) do
     user = @app_repo.get_by(@user_model, email: downcase(email))
-
+    case user do
+      nil -> {:error, :no_user}
+      _ -> write_new_credentials(email, password)
+    end
   end
+
+  defp write_new_credentials(email, password)
+    recovery_params = %{
+      to_string(@new_password) => Password.hash(password),
+      to_string(@verification_token) => Ecto.UUID.generate
+    }
+    changeset = change(user, recovery_params)
+    case @app_repo.update(changeset) do
+      {:ok, user} -> user
+      {:error, changeset} -> :error
+    end
+  end
+
+  
 end
